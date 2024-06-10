b625d6f26543048555567c1bf3b8c5b7851ed1ee
Yasin Cakal <whoisyasin@gmail.com>
2024-06-10 18:37:20 +0300

    finished lesson 3

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,22 @@
 print("Hello Python World!")
 
+# python3 --version
 # Type python3 to use the REPL and quit() to exit
+# Type python3 file_name.py to run it in terminal
+# Use SHIFT + CTRL + DOWN to copy paste to new line
+
+# 24 / 5 prints 4.8
+# 24 // 5 prints 4
+# round(24 / 5) prints 5
+
+# True and False is False
+# True or False is True
+
+grade = 80
+
+if grade > 65:
+  print("Passed!")
+else:
+  print("Failed!")
+
+print("Passed!") if grade > 65 else print("Failed!")

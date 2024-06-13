50be2e2e1c4dc1dcc0d8676ab2367034c8dfb0f0
Yasin Cakal <whoisyasin@gmail.com>
2024-06-13 12:28:26 +0300

    forloops

--- logic_loops.py
+++ logic_loops.py
@@ -4,3 +4,18 @@ elif False:
   print('False')
 else:
   print('Error')
+
+num_list = [4, 2, 6, 8, 1]
+for num in num_list:
+  print(f'The number in the list is {num}')
+
+for ch in 'Hello':
+  print(ch.upper())
+
+nested_list = [[0], [1, 2], [3, 4, 5]]
+for a, *b in nested_list:
+  print(a, b)
+
+d = {'k1':1, 'k2':2}
+for key, value in d.items():
+  print(key, value)

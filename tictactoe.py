f87cd228f3a09266a75b6fea20957bfa8892ead2
yasin1122 <whoisyasin@gmail.com>
2024-07-03 13:08:42 +0300

    fill choices

--- tictactoe.py
+++ tictactoe.py
@@ -6,9 +6,9 @@
 
 # print the board
 positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-player1_choices = []
-player2_choices = []
-choices = ['X', 'O']
+player_X_choices = []
+player_Y_choices = []
+choices = ['X', 'Y']
 horizontal_line = ' 1 | 2 | 3 '
 vertical_line = '-----------------'
 winning_positions = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
@@ -16,19 +16,15 @@ winning_positions = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
                      (0, 4, 8), (2, 4, 6)]
 
 # print the current board
-def print_board(choice):
-    positions[choice-1] = 'X'
+def print_board():
     for i in range(len(positions)):
         if i==2 or i==5:
             print(' ', positions[i], '  ')
             print(vertical_line)
         elif i==8:
-            print(' ', positions[i], '  \n\n')
+            print(' ', positions[i], '  ')
         else:
             print(' ', positions[i], end='  |')
-
-print_board(1)
-print_board(2)
 
 # import random
 # print(random.choice([1,2]))
@@ -44,4 +40,28 @@ def win_check(player_choices):
 
 win_check([8, 3, 4, 0, 7])
 
-print((1, 2, 3) not in [0, 1, 2, 3])
+# get user input and fill player choices
+def fill_choices(player_choices, ch):
+    user_input = None
+    prompt = "Enter an integer 1 through 9 (if not taken): "
+    while not user_input:
+        try:
+            print_board()
+            user_input = input(prompt)
+            user_input = int(user_input)
+            if user_input not in positions:
+                print(prompt)
+                user_input = None
+            else:
+                player_choices.append(user_input)
+                if ch == 'X':
+                    positions[user_input - 1] = 'X'
+                elif ch == 'Y':
+                    positions[user_input - 1] = 'Y'
+        except Exception as error:
+            print(error, prompt)
+            user_input = None
+
+fill_choices(player_X_choices, 'X')
+fill_choices(player_Y_choices, 'Y')
+print(player_X_choices, player_Y_choices)

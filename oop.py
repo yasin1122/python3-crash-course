c58dd5e4b15785195a2bc7886514c3d3c017ceba
yasin1122 <whoisyasin@gmail.com>
2024-07-04 09:57:27 +0300

    inheritance

--- oop.py
+++ oop.py
@@ -1,13 +1,25 @@
-class Bird():
-    # class object attribute
-    has_wings = True
+class Animal():
 
-    def __init__(self, breed) -> None:
+    def __init__(self) -> None:
+        print("Animal Created")
+    
+    def eat(self):
+        print("I am eating!")
+
+
+class Bird(Animal):
+    # class object attribute
+    wings = 2
+
+    def __init__(self, breed='unidentified') -> None:
+        Animal.__init__(self)
         self.breed = breed
 
     def fly(self, name):
-        print(f"{name}, a {self.breed}, is flying!")
+        print(f"{name}, a {self.breed}," + 
+              " is flying with its {Bird.wings} wings.")
 
 my_bird = Bird(breed='Parakeet')
 print(my_bird.breed)
 my_bird.fly("Tombik")
+my_bird.eat()

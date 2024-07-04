5c3552d3092a6f0eec54680f0df3cd4b4ee10253
yasin1122 <whoisyasin@gmail.com>
2024-07-04 19:58:19 +0300

    oop hw done

--- oop_hw.py
+++ oop_hw.py
@@ -14,3 +14,21 @@ class Line:
 c1, c2 = (3, 2), (8, 10)
 line1 = Line(c1, c2)
 print(line1.distance(), line1.slope())
+
+class Cylinder:
+
+    def __init__(self, height=1, radius=1) -> None:
+        self.height = height
+        self.radius = radius
+
+    # V=pi(r^2)h
+    def volume(self):
+        return 3.14*(self.radius**2)*self.height
+
+    # A=2pi(r^2) + 2pi(rh)
+    def surface_area(self):
+        return (2*3.14*(self.radius**2)+
+                2*3.14*self.radius*self.height)
+    
+cylinder1 = Cylinder(2, 3)
+print(cylinder1.volume(), cylinder1.surface_area())

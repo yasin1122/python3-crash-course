4c56f07dd734a758231466ff4555fcf02f48739e
yasin1122 <whoisyasin@gmail.com>
2024-07-04 09:44:49 +0300

    attributes

--- oop.py
+++ oop.py
@@ -1,6 +1,13 @@
 class Bird():
+    # class object attribute
+    has_wings = True
+
     def __init__(self, breed) -> None:
         self.breed = breed
 
+    def fly(self, name):
+        print(f"{name}, a {self.breed}, is flying!")
+
 my_bird = Bird(breed='Parakeet')
 print(my_bird.breed)
+my_bird.fly("Tombik")

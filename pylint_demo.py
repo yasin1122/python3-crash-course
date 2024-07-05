068dd6bef64397976bf75c986f1286ac68b5884e
yasin1122 <whoisyasin@gmail.com>
2024-07-05 21:41:21 +0300

    pylint demo restructured

---
+++ pylint_demo.py
@@ -0,0 +1,15 @@
+"""
+It is recommended that we use docstrings.
+These explain what the program does.
+"""
+
+def myfunc():
+    """
+    Docstrings are recommended for funtions as well.
+    """
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()

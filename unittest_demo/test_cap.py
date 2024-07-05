68094324f05145d2c0973b488c5df6474d895a2a
yasin1122 <whoisyasin@gmail.com>
2024-07-05 21:53:30 +0300

    wrote the test

--- unittest_demo/test_cap.py
+++ unittest_demo/test_cap.py
@@ -1,2 +1,28 @@
+"""
+This is the test script for cap.py
+"""
+
+# import statements
 import unittest
 import cap
+
+class TestCap(unittest.TestCase):
+
+    def test_one_word(self):
+        """
+        Tests cap_text with one word.
+        """
+        text = 'python'
+        result = cap.cap_text(text)
+        self.assertEqual(result, 'Python')
+
+    def test_multiple_words(self):
+        """
+        Tests cap_text with multiple words.
+        """
+        text = 'monty python'
+        result = cap.cap_text(text)
+        self.assertEqual(result, 'Monty Python')
+
+if __name__ == '__main__':
+    unittest.main()

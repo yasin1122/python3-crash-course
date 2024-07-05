46b0f7862bb3cde35f5cad8b2bd7b3a4d9f9cfb4
yasin1122 <whoisyasin@gmail.com>
2024-07-05 23:17:31 +0300

    generator demo

---
+++ generators.py
@@ -0,0 +1,10 @@
+def create_cubes(n):
+    for x in range(n):
+        yield x**3
+
+cube_list = list(create_cubes(10))
+print(cube_list)
+
+# Generators are memory efficient
+for x in create_cubes(10):
+    print(x, end=' ')

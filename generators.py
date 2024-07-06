69da98096dfae8dca5b12b5fe45dd2e4108b65a8
yasin1122 <whoisyasin@gmail.com>
2024-07-06 06:21:08 +0300

    july6

--- generators.py
+++ generators.py
@@ -2,10 +2,17 @@ def create_cubes(n):
     for x in range(n):
         yield x**3
 
-cube_list = list(create_cubes(10))
-print(cube_list)
-
 # Generators are memory efficient
 for x in create_cubes(10):
     print(x, end=' ')
+print()
 
+def gen_fib(n):
+    a, b = 1, 1
+    for i in range(n):
+        yield a
+        # tuple matching avoids temp var
+        a, b = b, a+b
+
+for i in gen_fib(5):
+    print(i, end=' ')

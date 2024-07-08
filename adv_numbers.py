bf8d60fbcd7806c23688df39747cfee085486ff8
yasin1122 <whoisyasin@gmail.com>
2024-07-08 12:16:53 +0300

    advanced strings

--- adv_numbers.py
+++ adv_numbers.py
@@ -1,3 +1,15 @@
+# Advanced Numbers
+
 print(hex(15)) # hexadecimal representation
 print(bin(15)) # binary representation
 print(pow(2, 4), abs(-11), round(3.141592, 2))
+
+# Advanced Strings
+
+s = 'hello world'
+print(s.capitalize(), s.upper(), s.count('o'),
+      s.find('o'), s.center(20, '*'), '\n',
+      s.isalnum(), s.isalpha(), s.islower(), 
+      s.isspace(), s.istitle(), s.isupper(),
+      s.endswith('d'), '\n', s.split('o'),
+      s.partition('o'))
